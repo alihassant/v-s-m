a5e7a6c93b9de832874a415e7829ba39696657eb
Ali Hassan <62383138+alihassant@users.noreply.github.com>
2024-09-30 04:58:17 +0500

    add database connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Using existing connection");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI || "");
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB Connected Successfully");
+  } catch (err) {
+    console.log("DB Connection Failed", err);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;

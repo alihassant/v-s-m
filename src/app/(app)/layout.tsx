534873b2f776d4c55e1e293a306b201d2471d794
Ali Hassan <62383138+alihassant@users.noreply.github.com>
2024-10-06 07:29:06 +0500

    fix some ui problems and update middleware

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,14 +1,7 @@
-import Navbar from "@/components/Navbar";
-
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 export default async function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <div>
-      <Navbar />
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -9,10 +9,14 @@ import {
 import AutoPlay from "embla-carousel-autoplay";
 import messages from "@/data/messages.json";
 import { Mail } from "lucide-react";
+import Navbar from "@/components/Navbar";
 
 const page = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
+      {" "}
+      <Navbar />
+      {/* Parent container */}
       <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
         <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
@@ -50,12 +54,11 @@ const page = () => {
           </CarouselContent>
         </Carousel>
       </main>
-
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
         © 2024 Vortaps Secret Message. All rights reserved.
       </footer>
-    </>
+    </div>
   );
 };
 

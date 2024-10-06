57e97fcd824d02d60cad00ad9d86cb4dbcc44d23
Ali Hassan <62383138+alihassant@users.noreply.github.com>
2024-10-06 05:34:04 +0500

    Update route.ts

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,27 +1,27 @@
-// import { streamText } from "ai";
-// import { createOpenAI as createGroq } from "@ai-sdk/openai";
+import { streamText } from "ai";
+import { createOpenAI as createGroq } from "@ai-sdk/openai";
 
-// const groq = createGroq({
-//   baseURL: "https://api.groq.com/openai/v1",
-//   apiKey: process.env.GROQ_API_KEY,
-// });
+const groq = createGroq({
+  baseURL: "https://api.groq.com/openai/v1",
+  apiKey: process.env.GROQ_API_KEY,
+});
 
-// const prompt =
-//   "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+const prompt =
+  "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-// export async function GET(req: Request) {
-//   const { messages } = await req.json();
+export async function GET(req: Request) {
+  const { messages } = await req.json();
 
-//   try {
-//     const textStream = await streamText({
-//       model: groq("llama-3.1-70b-versatile"),
-//       //   prompt: prompt,
-//       prompt: "Hello, how are you?",
-//     });
+  try {
+    const textStream = await streamText({
+      model: groq("llama-3.1-70b-versatile"),
+      prompt: prompt,
+      //   prompt: "Hello, how are you?",
+    });
 
-//     return textStream.toDataStreamResponse();
-//   } catch (error) {
-//     console.error(error);
-//     return new Response("Internal Server Error", { status: 500 });
-//   }
-// }
+    return textStream.toDataStreamResponse();
+  } catch (error) {
+    console.error(error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
